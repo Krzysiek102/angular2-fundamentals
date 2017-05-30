39c3e162520af0b16878b3e1b20d55794f98a84d
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2017-05-30 09:06:24 +0200

    Built in pipes

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -5,8 +5,8 @@ import { Component, Input } from '@angular/core'
     selector: 'event-thumbnail',
     template: `
         <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
-            <h2>{{event?.name}}</h2>
-            <div>Date: {{event?.date}}</div>
+            <h2>{{event?.name | uppercase}}</h2>
+            <div>Date: {{event?.date | date:'shortDate'}}</div>
             <div 
                 [ngClass]="getStartTimeClass()"
                 [ngSwitch]="event?.time">
@@ -15,7 +15,7 @@ import { Component, Input } from '@angular/core'
                 <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
                 <span *ngSwitchDefault>(Normal Start)</span>
             </div>
-            <div>Price: \${{ event?.price }}</div>
+            <div>Price: {{ event?.price | currency:'USD':true }}</div>
             <div *ngIf="event?.location">
                 <span>Location: {{event?.location?.address}}</span>
                 <span class="pad-left">
@@ -49,8 +49,8 @@ export class EventThumbnailComponent {
     @Input()
     event: IEvent
 
-    getStartTimeClass(){
+    getStartTimeClass() {
         const isEarlyStart = this.event && this.event.time === '8:00 am';
-        return {green: isEarlyStart, bold: isEarlyStart}
+        return { green: isEarlyStart, bold: isEarlyStart }
     }
 }

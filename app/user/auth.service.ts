f48e0c6593a199991cf187f484bad70824aa0775
Krzysiek102 <Krzysztof.Kurek102@gmail.com>
2017-06-06 07:43:37 +0200

    Authentication

--- app/user/auth.service.ts
+++ app/user/auth.service.ts
@@ -1,16 +1,32 @@
+import { Observable } from 'rxjs/Observable';
+import { Http, Headers, RequestOptions } from '@angular/http';
 import { IUser } from './user.model';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/observable/of';
 
 @Injectable()
 export class AuthService {
     currentUser: IUser;
+
+    constructor(private http: Http) {
+
+    }
+
     loginUser(userName: string, password: string) {
-        this.currentUser = {
-            id: 1,
-            userName: userName,
-            firstName: 'John',
-            lastName: 'Papa'
-        }
+        let headers = new Headers(
+            { 'Content-Type': 'application/json' }
+        );
+        let options = new RequestOptions({ headers: headers });
+        let loginInfo = { username: userName, password: password };
+        return this.http.post('/api/login', JSON.stringify(loginInfo), options)
+            .do(response => {
+                if (response) {
+                    this.currentUser = <IUser>response.json().user;
+                }
+            }).catch(error => {
+                return Observable.of(false);
+            });
     }
 
     isAuthenticated() {
